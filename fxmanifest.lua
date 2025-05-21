8eccfe7d327a8d91ad26858452350f734c16dbca
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-20 22:48:41 -0500

    overhaul formatting of files to better match current script structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,28 +1,30 @@
 fx_version "cerulean"
 
-description "Turf Script"
+description "Boilerplate for FiveM resources"
 author "Lebubble Scripts"
-version '1.0.0-alpha'
+version '1.0.0'
 
 lua54 'yes'
 
 game "gta5"
 
-ui_page 'web/build/index.html'
+-- Use below if you want to use a NUI interface
+--ui_page 'web/build/index.html'
 
 shared_script  {
-  'config.lua',
-  '@ox_lib/init.lua'
+  'shared/config.lua',
+  --'@ox_lib/init.lua'
 }
 
-dependencies {
-  'ox_lib',
-  'oxmysql'
-}
+-- Uncomment below if you will use oxmysql or ox-lib
+--dependencies {
+--  'ox_lib',
+--  'oxmysql'
+--}
 
 client_script "client/**/*"
 
-server_scripts {
+server_scripts {  
   -- Uncomment below if you will use oxmysql
   --'@oxmysql/lib/MySQL.lua',
   "server/**/*"
